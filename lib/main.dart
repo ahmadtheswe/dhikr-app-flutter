f6ef3389b5819a3ff15b2bb0e66813bd320c45f2
ahmadtheswe <ahmadm.abdurrahman@gmail.com>
2025-04-07 12:32:42 +0700

    update dhikr for english

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: languageService.currentLanguage != null ? const SelectDhikrTimePage() : const SelectLanguagePage(),
     );
   }
